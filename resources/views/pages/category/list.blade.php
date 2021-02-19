4f8fe6f8c7a96108f62dc5a01e9ed05357843f3c
Allen <allen@localhost>
2021-02-19 11:16:01 +0200

    add  table for category and tag

--- resources/views/pages/category/list.blade.php
+++ resources/views/pages/category/list.blade.php
@@ -14,10 +14,21 @@
     @forelse($categories as $category)
         @if ($loop->first)
           <table  class="table table-striped">
+              <thead>
+              <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Slug</th>
+                  <th scope="col">Create at</th>
+                  <th scope="col">Updated at</th>
+                  <th scope="col">Delete</th>
+                  <th scope="col">Update</th>
+              </tr>
+              </thead>
         @endif
 
-              <tr><td>{{ $category->title }}
-                  </td><td><form action="/category/{{$category->id}}/delete" method="get">
+              <tr><td>{{$category->id}}</td><td>{{ $category->title }}
+                  </td><td>{{$category->slug}}</td><td>{{$category->created_at}}</td><td>{{$category->updated_at}}</td><td><form action="/category/{{$category->id}}/delete" method="get">
                           <input type="submit" value="удалить"></form></td><td><form action="/category/{{$category->id}}/update" method="get">
                           <input type="submit" value="изменить"></form></td></tr>
 
--- resources/views/pages/tag/list.blade.php
+++ resources/views/pages/tag/list.blade.php
@@ -12,16 +12,27 @@
     @endif
     @forelse($tags as $tag)
         @if ($loop->first)
-          <table  class="table table-striped">
-        @endif
+            <table  class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Create at</th>
+                    <th scope="col">Updated at</th>
+                    <th scope="col">Delete</th>
+                    <th scope="col">Update</th>
+                </tr>
+                </thead>
+                @endif
 
-              <tr><td>{{ $tag->title }}
-                  </td><td><form action="/tag/{{$tag->id}}/delete" method="get">
-                          <input type="submit" value="удалить"></form></td><td><form action="/tag/{{$tag->id}}/update" method="get">
-                          <input type="submit" value="изменить"></form></td></tr>
+                <tr><td>{{$tag->id}}</td><td>{{ $tag->title }}
+                    </td><td>{{$tag->slug}}</td><td>{{$tag->created_at}}</td><td>{{$tag->updated_at}}</td><td><form action="/tag/{{$tag->id}}/delete" method="get">
+                            <input type="submit" value="удалить"></form></td><td><form action="/tag/{{$tag->id}}/update" method="get">
+                            <input type="submit" value="изменить"></form></td></tr>
 
-        @if ($loop->last)
-          </table>
+                @if ($loop->last)
+            </table>
         @endif
     @empty
         <p>no tags</p>
